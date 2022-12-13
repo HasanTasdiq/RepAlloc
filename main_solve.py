bb754967c14e8d1fe174c701d5c7aab9aa422c31
Tasdiqul <tasdiq088@gmail.com>
2022-12-13 10:14:38 -0800

    es net

--- main_solve.py
+++ main_solve.py
@@ -117,7 +117,7 @@ def solve_gml(network_name):
 
 if __name__ == "__main__":
     # surfnet_solve()
-    solve_gml('Atmnet.gml')
+    solve_gml('es_net.gml')
     # solve_from_gml("Colt.gml", L_max=900, N_max=6, D=6, K=1, alpha=1 / 75000)
     # solve_on_unit_cube(L_max=0.9, N_max=3, D=6, K=1)
     # solve_with_random_graph()
